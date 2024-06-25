7378f4a94662b6b9dbdee8f1a4b8d59c50bfd6d0
mmishinav <dortv18@gmail.com>
2024-06-25 16:57:04 +0300

    add swagger and redoc api documentation

--- server/transaction/views.py
+++ server/transaction/views.py
@@ -1,12 +1,98 @@
 from rest_framework import response, decorators as rest_decorators, permissions as rest_permissions
 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+
+@swagger_auto_schema(
+    method="POST",
+    operation_summary="Pay for a subscription",
+    operation_description="Processes the payment for a subscription.",
+    responses={
+        200: openapi.Response(
+            description="Payment processed successfully",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "msg": openapi.Schema(type=openapi.TYPE_STRING, example="Success"),
+                },
+            ),
+        ),
+        401: openapi.Response(
+            description="Unauthorized",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Authentication credentials were not provided or are invalid",
+                    ),
+                },
+            ),
+        ),
+        403: openapi.Response(
+            description="Permission denied",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Forbidden",
+                        description="Error message indicating insufficient permissions."
+                    )
+                }
+            )
+        ),
+    },
+)
 @rest_decorators.api_view(["POST"])
 @rest_decorators.permission_classes([rest_permissions.IsAuthenticated])
 def paySubscription(request):
     return response.Response({"msg": "Success"}, 200)
 
 
+@swagger_auto_schema(
+    method="POST",
+    operation_summary="Retrieve subscriptions",
+    operation_description="Retrieves the list of all subscriptions for the user.",
+    responses={
+        200: openapi.Response(
+            description="Success",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "msg": openapi.Schema(type=openapi.TYPE_STRING, example="Success"),
+                },
+            ),
+        ),
+        401: openapi.Response(
+            description="Unauthorized",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Authentication credentials were not provided or are invalid",
+                        description="Error message indicating missing or invalid authentication credentials."
+                    )
+                }
+            )
+        ),
+        403: openapi.Response(
+            description="Permission denied",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Forbidden",
+                        description="Error message indicating insufficient permissions."
+                    )
+                }
+            )
+        ),
+    },
+)
 @rest_decorators.api_view(["POST"])
 @rest_decorators.permission_classes([rest_permissions.IsAuthenticated])
 def listSubscriptions(request):
--- server/user/views.py
+++ server/user/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import authenticate
 from django.conf import settings
 from django.middleware import csrf
-from rest_framework import exceptions as rest_exceptions, response, decorators as rest_decorators, permissions as rest_permissions
-from rest_framework_simplejwt import tokens, views as jwt_views, serializers as jwt_serializers, exceptions as jwt_exceptions
+from rest_framework import exceptions as rest_exceptions, response, decorators as rest_decorators, \
+    permissions as rest_permissions
+from rest_framework_simplejwt import tokens, views as jwt_views, serializers as jwt_serializers, \
+    exceptions as jwt_exceptions
 from user import serializers, models
 import stripe
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 prices = {
@@ -25,6 +30,51 @@ def get_user_tokens(user):
     }
 
 
+@swagger_auto_schema(
+    method="POST",
+    operation_summary="User Login",
+    operation_description="Authenticate user and issue JWT tokens. Sets cookies for access and refresh tokens.",
+    request_body=serializers.LoginSerializer,
+    responses={
+        200: openapi.Response(
+            description="Login successful",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "refresh_token": openapi.Schema(type=openapi.TYPE_STRING, example="refresh_token",
+                                                    description="JWT refresh token"),
+                    "access_token": openapi.Schema(type=openapi.TYPE_STRING, example="access_token",
+                                                   description="JWT access token")
+                },
+            ),
+        ),
+        400: openapi.Response(
+            description="Bad Request",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Email or Password is incorrect!",
+                    ),
+                },
+            ),
+        ),
+        401: openapi.Response(
+            description="Unauthorized",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Authentication credentials were not provided or are invalid",
+                        description="Error message indicating missing or invalid authentication credentials."
+                    )
+                }
+            )
+        ),
+    },
+)
 @rest_decorators.api_view(["POST"])
 @rest_decorators.permission_classes([])
 def loginView(request):
@@ -64,6 +114,53 @@ def loginView(request):
         "Email or Password is incorrect!")
 
 
+@swagger_auto_schema(
+    method="POST",
+    operation_summary="User Registration",
+    operation_description="Register a new user.",
+    request_body=serializers.RegistrationSerializer,
+    responses={
+        201: openapi.Response(
+            description="Registration successful",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Registered!",
+                        description="Confirmation message upon successful registration."
+                    )
+                }
+            )
+        ),
+        400: openapi.Response(
+            description="Bad Request",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Invalid credentials!",
+                        description="Error message indicating invalid registration credentials."
+                    )
+                }
+            )
+        ),
+        401: openapi.Response(
+            description="Unauthorized",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Authentication credentials were not provided or are invalid",
+                        description="Error message indicating missing or invalid authentication credentials."
+                    )
+                }
+            )
+        ),
+    },
+)
 @rest_decorators.api_view(["POST"])
 @rest_decorators.permission_classes([])
 def registerView(request):
@@ -77,6 +174,65 @@ def registerView(request):
     return rest_exceptions.AuthenticationFailed("Invalid credentials!")
 
 
+@swagger_auto_schema(
+    method="POST",
+    operation_summary="User Logout",
+    operation_description="Blacklists the refresh token to invalidate it and deletes cookies.",
+    responses={
+        200: openapi.Response(
+            description="Logout successful",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Logout successful.",
+                        description="Confirmation message upon successful logout."
+                    )
+                }
+            )
+        ),
+        400: openapi.Response(
+            description="Bad Request",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Invalid token",
+                        description="Error message indicating invalid token format or value."
+                    )
+                }
+            )
+        ),
+        401: openapi.Response(
+            description="Unauthorized",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Authentication credentials were not provided or are invalid",
+                        description="Error message indicating missing or invalid authentication credentials."
+                    )
+                }
+            )
+        ),
+        403: openapi.Response(
+            description="Permission denied",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Forbidden",
+                        description="Error message indicating insufficient permissions."
+                    )
+                }
+            )
+        ),
+    },
+)
 @rest_decorators.api_view(['POST'])
 @rest_decorators.permission_classes([rest_permissions.IsAuthenticated])
 def logoutView(request):
@@ -91,8 +247,8 @@ def logoutView(request):
         res.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
         res.delete_cookie("X-CSRFToken")
         res.delete_cookie("csrftoken")
-        res["X-CSRFToken"]=None
-        
+        res["X-CSRFToken"] = None
+
         return res
     except:
         raise rest_exceptions.ParseError("Invalid token")
@@ -128,7 +284,93 @@ class CookieTokenRefreshView(jwt_views.TokenRefreshView):
         response["X-CSRFToken"] = request.COOKIES.get("csrftoken")
         return super().finalize_response(request, response, *args, **kwargs)
 
+    @swagger_auto_schema(
+        operation_summary="Token Refresh with Cookie",
+        operation_description="Customizes the token refresh view to set a new refresh token in a cookie and "
+                              "return the access token. Includes CSRF token in the response headers.",
+        responses={
+            200: openapi.Response(
+                description="Token refresh successful",
+                schema=CookieTokenRefreshSerializer,
+            ),
+            400: openapi.Response(
+                description="Bad Request",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        "detail": openapi.Schema(
+                            type=openapi.TYPE_STRING,
+                            example="Invalid token",
+                            description="Error message indicating invalid token format or value."
+                        )
+                    }
+                )
+            ),
+            401: openapi.Response(
+                description="Unauthorized",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        "detail": openapi.Schema(
+                            type=openapi.TYPE_STRING,
+                            example="Authentication credentials were not provided or are invalid",
+                            description="Error message indicating missing or invalid authentication credentials."
+                        )
+                    }
+                )
+            ),
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
+
+@swagger_auto_schema(
+    method="GET",
+    operation_summary="Retrieve User Details",
+    operation_description="Retrieve details of the authenticated user.",
+    responses={
+        200: openapi.Response(
+            description="Successful retrieval of user details",
+            schema=serializers.UserSerializer,
+        ),
+        401: openapi.Response(
+            description="Unauthorized",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Authentication credentials were not provided or are invalid",
+                        description="Error message indicating missing or invalid authentication credentials."
+                    )
+                }
+            )
+        ),
+        403: openapi.Response(
+            description="Permission denied",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Forbidden",
+                        description="Error message indicating insufficient permissions."
+                    )
+                }
+            )
+        ),
+        404: openapi.Response(
+            description="User not found",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(type=openapi.TYPE_STRING, example="User not found")
+                }
+            )
+        ),
+    }
+)
 @rest_decorators.api_view(["GET"])
 @rest_decorators.permission_classes([rest_permissions.IsAuthenticated])
 def user(request):
@@ -141,6 +383,68 @@ def user(request):
     return response.Response(serializer.data)
 
 
+@swagger_auto_schema(
+    method="GET",
+    operation_summary="Retrieve User Subscriptions",
+    operation_description="Retrieve active subscriptions of the authenticated user from Stripe.",
+    responses={
+        200: openapi.Response(
+            description="Successful retrieval of user subscriptions",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "subscriptions": openapi.Schema(
+                        type=openapi.TYPE_ARRAY,
+                        items=openapi.Schema(
+                            type=openapi.TYPE_OBJECT,
+                            properties={
+                                "id": openapi.Schema(type=openapi.TYPE_STRING, example="s123"),
+                                "start_date": openapi.Schema(type=openapi.TYPE_STRING, format="date",
+                                                             example="2023-06-25"),
+                                "plan": openapi.Schema(type=openapi.TYPE_STRING, example="p123")
+                            }
+                        )
+                    )
+                }
+            )
+        ),
+        401: openapi.Response(
+            description="Unauthorized",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Authentication credentials were not provided or are invalid",
+                        description="Error message indicating missing or invalid authentication credentials."
+                    )
+                }
+            )
+        ),
+        403: openapi.Response(
+            description="Permission denied",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        example="Forbidden",
+                        description="Error message indicating insufficient permissions."
+                    )
+                }
+            )
+        ),
+        404: openapi.Response(
+            description="User not found",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "detail": openapi.Schema(type=openapi.TYPE_STRING, example="User not found")
+                }
+            )
+        )
+    }
+)
 @rest_decorators.api_view(["GET"])
 @rest_decorators.permission_classes([rest_permissions.IsAuthenticated])
 def getSubscriptions(request):
